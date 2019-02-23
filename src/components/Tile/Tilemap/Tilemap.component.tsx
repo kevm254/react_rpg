99fe0c0d7055ca63ca2af87a2c2dad6562b7c3bd
kevm254 <kevm2@yahoo.com>
2019-02-22 23:37:04 -0600

    added tilemap
    added tilemap and sprite

---
+++ src/components/Tile/Tilemap/Tilemap.component.tsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+export default class Tilemap extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getTile(number) {
+    switch (number) {
+      case 1:
+        return "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7cS6HEDK-fEvCFDP2rx2B1xaHogF3HvlfWJQslbQun7SDOp-c";
+      case 2:
+        return "http://www.textures4photoshop.com/tex/thumbs/sand-with-pebbles-texture-high-res-thumb34.jpg";
+      case 3:
+        return "https://img-aws.ehowcdn.com/350x235p/photos.demandstudios.com/getty/article/178/191/78325586_XS.jpg";
+    }
+  }
+  render() {
+    return (
+      <div
+        style={{
+          display: this.props.row ? "inline-block" : "block",
+          backgroundColor: this.props.color || "blue",
+          height: 64,
+          width: 64,
+          border: this.props.showBorder ? "1px solid white" : "",
+          overflow: "hidden",
+          marginTop: "-4px"
+        }}
+      >
+        <img src={this.getTile(this.props.tileNo)} />
+      </div>
+    );
+  }
+}
--- src/states/debug/debug.state.tsx
+++ src/states/debug/debug.state.tsx
@@ -1,12 +1,132 @@
 import React, { Component } from "react";
 import TextBox from "../../components/UI/TextBox/TextBox";
+import Tile from "../../components/Tile/Tilemap/Tilemap.component";
+import anime from "animejs";
+import { Keys } from "../../Models/keys.model";
 
 export default class DebugState extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerXPos: 0,
+      playerYPos: 0
+    };
+  }
+  playerRef = React.createRef();
+
+  data = [
+    [1, 3, 1, 2, 1, 1, 1, 2, 1, 1],
+    [1, 3, 1, 1, 1, 2, 1, 2, 1, 1],
+    [1, 1, 1, 1, 1, 2, 1, 2, 1, 1],
+    [3, 3, 3, 3, 1, 1, 1, 2, 1, 1],
+    [1, 2, 2, 2, 1, 2, 1, 2, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 2, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 2, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 2, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 2, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 2, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 2, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 2, 1, 1]
+  ];
+
+  obstructionMap = [
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+
+  tiles = [];
+
+  componentWillMount() {
+    this.renderRow(this.data);
+
+    console.log(this.tiles);
+  }
+
+  componentDidMount() {
+    document.body.addEventListener("keydown", e => {
+      if (e.keyCode === Keys.RIGHT) {
+        if (
+          this.obstructionMap[this.state.playerYPos][
+            this.state.playerXPos + 1
+          ] !== 1
+        ) {
+          this.setState({ playerXPos: this.state.playerXPos + 1 });
+          anime({
+            targets: this.playerRef.current,
+            translateX: 64 * this.state.playerXPos,
+            easing: "linear"
+          });
+        }
+      }
+      if (e.keyCode === Keys.LEFT) {
+        this.setState({ playerXPos: this.state.playerXPos - 1 });
+        anime({
+          targets: this.playerRef.current,
+          translateX: 64 * this.state.playerXPos,
+          easing: "linear"
+        });
+      }
+      if (e.keyCode === Keys.UP) {
+        this.setState({ playerYPos: this.state.playerYPos - 1 });
+        anime({
+          targets: this.playerRef.current,
+          translateY: 64 * this.state.playerYPos,
+          easing: "linear"
+        });
+      }
+
+      if (e.keyCode === Keys.DOWN) {
+        this.setState({ playerYPos: this.state.playerYPos + 1 });
+        anime({
+          targets: this.playerRef.current,
+          translateY: 64 * this.state.playerYPos,
+          easing: "linear"
+        });
+      }
+    });
+  }
+
+  renderRow(dataSource, row: boolean) {
+    dataSource.forEach(row => {
+      this.tiles.push(
+        row.map((item, i) => {
+          return (
+            <Tile color="green" row={true} tileNo={item} showBorder={false} />
+          );
+        }),
+        <br />
+      );
+    });
+  }
   render() {
     return (
-      <div style={{ backgroundColor: "#000000", color: "#FFFFFF" }}>
+      <div style={{ backgroundColor: "#000000", color: "#FFFFFF" }} id="player">
         <h1>Debug Screen</h1>
-        <TextBox />
+        <div style={{ position: "fixed", right: "50px" }}>
+          PlayerPos: [{this.state.playerXPos}, {this.state.playerYPos}]
+        </div>
+        <div
+          style={{
+            height: "64px",
+            width: "64px",
+            borderRadius: "100px",
+            backgroundColor: "blue",
+            position: "absolute"
+          }}
+          ref={this.playerRef}
+        />
+        {this.tiles}
       </div>
     );
   }
